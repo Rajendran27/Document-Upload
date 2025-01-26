f7ad5cb4f9fab49c2387e963b2aed9806a599988
Rajendran27 <rajendran2741@gmail.com>
2025-01-26 17:55:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+// import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home';
+
+
+
+
+function App() {
+  return (
+   <div className='container'>
+    {/* < Navbar /> */}
+    <Home />
+    
+   </div>
+  );
+}
+
+export default App;
